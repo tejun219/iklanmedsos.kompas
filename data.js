0403a371faa1062b1880cae20698db9c4b0cc559
Yustinus Sulistyono <yustinus.sulistyono@kompas.com>
2026-06-10 11:41:10 +0700

    Auto-update outline data: 2026-06-10 11:41:09

--- data.js
+++ data.js
@@ -5226,7 +5226,7 @@ const INITIAL_DATA = {
       "tgl_terbit": "2025-12-24",
       "ae": "Eka",
       "keterangan_order": "Memo No.867/MO/PRODUK/XII/2025",
-      "keterangan": 1,
+      "keterangan": 1.0,
       "so": "SO0117657",
       "row_idx": 41
     },
@@ -5262,7 +5262,7 @@ const INITIAL_DATA = {
       "tgl_terbit": "2025-12-24",
       "ae": "Eka",
       "keterangan_order": "Memo No.867/MO/PRODUK/XII/2025",
-      "keterangan": 2,
+      "keterangan": 2.0,
       "so": "SO0117658",
       "row_idx": 44
     },
@@ -5274,7 +5274,7 @@ const INITIAL_DATA = {
       "tgl_terbit": "2025-12-24",
       "ae": "Eka",
       "keterangan_order": "Memo No.867/MO/PRODUK/XII/2025",
-      "keterangan": 3,
+      "keterangan": 3.0,
       "so": "SO0117659",
       "row_idx": 45
     },
@@ -5310,7 +5310,7 @@ const INITIAL_DATA = {
       "tgl_terbit": "2025-12-30",
       "ae": "Eka",
       "keterangan_order": "Memo No.867/MO/PRODUK/XII/2025",
-      "keterangan": 4,
+      "keterangan": 4.0,
       "so": "SO0117660",
       "row_idx": 48
     },
@@ -5322,7 +5322,7 @@ const INITIAL_DATA = {
       "tgl_terbit": "2025-12-30",
       "ae": "Eka",
       "keterangan_order": "Memo No.867/MO/PRODUK/XII/2025",
-      "keterangan": 5,
+      "keterangan": 5.0,
       "so": "SO0117661",
       "row_idx": 49
     },
@@ -7722,8 +7722,8 @@ const INITIAL_DATA = {
       "tgl_terbit": "2026-06-09",
       "ae": "Imel",
       "keterangan_order": "Memo No.342/MO/PRODUK/VI/2026",
-      "keterangan": "https://www.instagram.com/stories/hariankompas/3915710149889858725?utm_source=ig_story_item_share&igsh=MjdpZDE2cXpzM2l1",
-      "so": "",
+      "keterangan": "",
+      "so": "SO0121447",
       "row_idx": 9
     },
     {
@@ -7759,7 +7759,7 @@ const INITIAL_DATA = {
       "ae": "Imel",
       "keterangan_order": "Memo No.342/MO/PRODUK/VI/2026",
       "keterangan": "",
-      "so": "",
+      "so": "SO0121448",
       "row_idx": 12
     }
   ],
