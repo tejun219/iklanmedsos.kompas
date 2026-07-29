490c89bf32390df2a97b6d7d82c81add3e9769e0
Yustinus Sulistyono <yustinus.sulistyono@kompas.com>
2026-07-29 14:49:36 +0700

    Update data iklan & memo - 29-07-2026 14:49

--- data.js
+++ data.js
@@ -9280,7 +9280,7 @@ const MEMO_MAP = {
   "356": "356 - SUPPLEMENT ALBAYRAK MEDIA GROUP update 17 Juli'26.pdf",
   "444": "444 - Djarum Bakti Budaya.pdf",
   "463": "463 - UKSW (Naura).pdf",
-  "448": "448 - UNIQLO update 28 juli'26.pdf"
+  "448": "448 - UNIQLO update 29 juli'26.pdf"
 };
 
 // Make available via window object for dynamic script loading compatibility
