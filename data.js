203fd2db9abdb644b2848916105084391af51160
Yustinus Sulistyono <yustinus.sulistyono@kompas.com>
2026-09-09 16:37:53 +0700

    Auto-upload saat buka dashboard - 09-09-2026 16:37

--- data.js
+++ data.js
@@ -10440,7 +10440,8 @@ const MEMO_MAP = {
   "047": "Barter 047 -  KAI EXPO update 4 sept'26.pdf",
   "47": "Barter 047 -  KAI EXPO update 4 sept'26.pdf",
   "689": "Memo No.689.pdf",
-  "694": "694 Event Bank Jateng BOMAR 2026.pdf"
+  "694": "694 Event Bank Jateng BOMAR 2026.pdf",
+  "581": "581 - Megapro (Djarum).pdf"
 };
 
 // Make available via window object for dynamic script loading compatibility
