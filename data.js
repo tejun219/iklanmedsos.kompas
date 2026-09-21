e57ddcc13814f3da6763f6a27e8a3c5ada80b7e8
Yustinus Sulistyono <yustinus.sulistyono@kompas.com>
2026-09-21 11:44:02 +0700

    Update data iklan & memo - 21-09-2026 11:44

--- data.js
+++ data.js
@@ -10529,7 +10529,7 @@ const MEMO_MAP = {
   "506": "506 - Komdigi update 18 agst'26.pdf",
   "443": "443 - DJARUM (HUT RI) update 18 agst'26.pdf",
   "405": "405 - Travellane update 21 agst'26.pdf",
-  "513": "513 - Kemenpar update 24 agst'26.pdf",
+  "513": "513 - Kemenpar update 21 sept'26.pdf",
   "510": "510 - PUMA & SOCIOLLA update 24 agst'26.pdf",
   "552": "552 - CSI (Bank Syariah Indonesia).pdf",
   "480": "480 - Bayan Resources update 3 sept'26.pdf",
